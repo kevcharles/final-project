6024819566e2797354828c91dc7567716e6e4430
JasonBCIT <40413227+JasonBCIT@users.noreply.github.com>
2018-06-24 18:28:31 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php
+	include ("header.php");
+?>
+    <article class="instructor">
+        <h1>The Instructor:Michael Whyte</h1>
+        <img src="images/michaelwhyte.jpg" alt="Michael Whyte">
+        <p>Working as an Instructor for BCIT's Technical Web Designer program since 2010, I instruct in various courses at the college level. As part of a team of instructors we also develop course curriculum and assist with the backend operations of the program. Specialties: Teaching & Front-End Web Development</p>
+    </article>
+
+<?php
+    include ("footer.php");
+?> 
---
+++ lessons.php
@@ -0,0 +1,24 @@
+<?php
+	include ("header.php");
+?>
+    <article>
+        <h1>Course Lessons</h1>
+        <ol>
+            <li><a href="lesson1.php">Lesson 1</a></li>
+            <li><a href="lesson2.php">Lesson 2</a></li>
+            <li><a href="lesson3.php">Lesson 3</a></li>
+            <li><a href="#0">Lesson 4</a></li>
+            <li><a href="#0">Lesson 5</a></li>
+            <li><a href="#0">Lesson 6</a></li>
+            <li><a href="#0">Lesson 7</a></li>
+            <li><a href="#0">Lesson 8</a></li>
+            <li><a href="#0">Lesson 9</a></li>
+            <li><a href="#0">Lesson 10</a></li>
+            <li><a href="#0">Lesson 11</a></li>
+            <li><a href="#0">Lesson 12</a></li>
+        </ol>
+    </article>
+
+<?php
+    include ("footer.php");
+?> 
