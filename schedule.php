59496ec7f3cb45e00db34d486607e1edc4642a0f
Kevin Charles <kcharles.kc4@gmail.com>
2018-06-27 21:26:45 -0700

    created schedule.php

---
+++ schedule.php
@@ -0,0 +1,106 @@
+<?php
+	include ("header.php");
+?>
+<div class="wrapper container">
+    <main>
+        <div class="schedule">
+            <h1>Schedule</h1>
+                    <div class="days"> <h2>Day 01</h2>
+                    <ul>
+                        <li>Course Introduction</li>
+                        <li>Setup development environment</li>
+                        <li>FTP Setup</li>
+                        <li>HTML and CSS Review HTML5 Video and Audio</li>
+                        <li>Assignment 1</li>
+                    </ul>
+                    </div>
+                    <div class="days">
+                    <h2>Day 02</h2>
+                    <ul>
+                        <li>CSS @font-face</li>
+                        <li>Responsive web design</li>
+                        <li>Assignment 2</li>
+                    </ul>
+                    </div>
+                    <div class="days">
+                    <h2>Day 03</h2>
+                    <ul>
+                        <li>Flexbox</li>
+                        <li>Assignment 3</li>
+                    </ul>
+                    </div>
+                    <div class="days">
+                    <h2>Day 04</h2>
+                    <ul>
+                        <li>CSS3 styling</li>
+                        <li>CSS3 selectors</li>
+                        <li>CSS transitions and animations</li>
+                        <li>Assignment 4</li>
+                    </ul>
+                    </div>
+                    <div class="days">
+                    <h2>Day 05</h2>
+                    <ul>
+                        <li>Responsive Images</li>
+                        <li>Mobile navigation systems</li>
+                        <li>Assignment 5</li>
+                        <li>Midterm project</li>
+                    </ul>
+                    </div>
+                    <div class="days">
+                    <h2>Day 06</h2>
+                    <ul>
+                        <li>Midterm Exam</li>
+                        <li>Lab time</li>
+                    </ul>
+                    </div>
+                    <div class="days">
+                    <h2>Day 07</h2>
+                    <ul>
+                        <li>CSS Grid</li>
+                        <li>Assignment 6</li>
+                    </ul>
+                    </div>
+                    <div class="days">
+                    <h2>Day 08</h2>
+                    <ul>
+                        <li>SASS</li>
+                        <li>Assignment 7</li>
+                    </ul>
+                    </div>
+                    <div class="days">
+                    <h2>Day 09</h2>
+                    <ul>
+                        <li>Git and GitHub</li>
+                        <li>Final Group Project</li>
+                    </ul>
+                    </div>
+                    <div class="days">
+                   <h2>Day 10</h2>
+                    <ul>
+                        <li>Servers</li>
+                        <li>htaccess</li>
+                        <li>PHP review</li>
+                        <li>JavaScript review</li>
+                    </ul>
+                    </div>
+                    <div class="days">
+                    <h2>Day 11</h2>
+                    <ul>
+                        <li>Lab time</li>
+                    </ul>
+                    </div>
+                    <div class="days">
+                    <h2>Day 12</h2>
+                    <ul>
+                        <li>Final Exam</li>
+                        <li>Lab time</li>
+                    </ul>
+            </div>
+      
+        </div>
+    </main>
+
+<?php
+    include ("footer.php");
+?> 
