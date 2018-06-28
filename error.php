07b0c822de9f2731d40bb41fe6333864734bbfb6
Kevin Charles <kcharles.kc4@gmail.com>
2018-06-27 22:03:47 -0700

    fixed error.php and thanks.php page interference with nav

--- error.php
+++ error.php
@@ -1,6 +1,7 @@
 <?php
 	include ("header.php");
 ?>
+<div class="wrapper container">
 <main> 
 <h2 style="text-align: center; padding: 20px;">We're sorry. There was an error delivering your message.  Our team is working quickly to resolve the problem. Please try again later. </h1>
 </main>
--- thanks.php
+++ thanks.php
@@ -1,6 +1,7 @@
 <?php
 	include ("header.php");
 ?>
+<div class="wrapper container">
 <main>
 <h2 style="text-align: center; padding: 20px;">Thanks!</h1>
 	</main>
